b169534c1cbcbe0676adcea0bd1a0475b73ffe37
marcosvillabasa <marcosvillabasa@gmail.com>
2018-10-12 11:30:39 -0300

    Cuarta etapa, creacion de una db en Firebase, crear servicio, mostrar en el componente

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,18 +8,34 @@ export class InfoPaginaService {
 
   info: infoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
 
-
     console.log("Servicio info pagina listo");
+    this.cargarInfo();
+    this.cargarEquipo();
 
-    //leer archivo json
-    this.http.get('assets/data/data-page.json')
-      .subscribe( (resp: infoPagina) => {
-        this.cargada=true;
-        this.info = resp;
+
+ }
+
+ //leer archivo json
+ private cargarInfo(){
+
+   this.http.get('assets/data/data-page.json')
+   .subscribe( (resp: infoPagina) => {
+     this.cargada=true;
+     this.info = resp;
+     console.log(resp);
+   });
+ }
+
+ private cargarEquipo(){
+    this.http.get('https://agular-html.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo=resp;
         console.log(resp);
       });
  }
+
 }
