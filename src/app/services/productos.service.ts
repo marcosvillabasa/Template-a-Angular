b5909b98761cb15eca41d99410642fd4af2720a5
marcosvillabasa <marcosvillabasa@gmail.com>
2018-10-19 11:22:35 -0300

    Listo para produccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -59,7 +59,7 @@ export class ProductosService {
       }
 
       private filtrarProductos (termino: string){
-          console.log(this.productos);
+          // console.log(this.productos);
           this.productosFiltrado = [];
 
           termino = termino.toLowerCase();
